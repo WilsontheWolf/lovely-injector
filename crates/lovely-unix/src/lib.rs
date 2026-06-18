16ca6869537978327e41b7a811c4518b9562661c
WilsontheWolf <git@shorty.systems>
2026-06-18 14:29:46 -0600

    handle panics better

--- crates/lovely-unix/src/lib.rs
+++ crates/lovely-unix/src/lib.rs
@@ -57,11 +57,6 @@ unsafe extern "C" fn lua_loadbufferx_detour(
 
 #[ctor::ctor]
 unsafe fn construct() {
-    panic::set_hook(Box::new(|x| {
-        let message = format!("lovely-injector has crashed: \n{x}");
-        error!("{message}");
-    }));
-
     let args: Vec<_> = env::args().collect();
 
     if args.contains(&"--vanilla".to_string()) || args.contains(&"-v".to_string()) {
