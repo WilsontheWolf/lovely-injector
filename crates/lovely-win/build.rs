7a060037bac31a8106b9ca2278f306b024541a5b
WilsontheWolf <git@shorty.systems>
2026-08-02 21:33:46 -0600

    whatever I was doing

--- crates/lovely-win/build.rs
+++ crates/lovely-win/build.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     let system_drive = env::var("SYSTEMDRIVE").unwrap_or("C:".to_string());
-    let windows_path = format!("{}\\Windows\\System32\\version.dll", system_drive);
+    let windows_path = format!("{}\\Windows\\System32\\winmm.dll", system_drive);
 
     if let Ok(dev_path) = env::var("LOVELY_VERSION_PATH") {
         forward_dll::forward_dll_with_dev_path(&windows_path, &dev_path).unwrap();
@@ -10,6 +10,6 @@ fn main() {
         #[cfg(target_os = "windows")]
         forward_dll::forward_dll(&windows_path).unwrap();
         #[cfg(not(target_os = "windows"))]
-        forward_dll::forward_dll_with_dev_path(&windows_path, "../../version.dll").unwrap();
+        forward_dll::forward_dll_with_dev_path(&windows_path, "../../winmm.dll").unwrap();
     }
 }
